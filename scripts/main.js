4de96ad132527ed5426b3639f247d408504e97b3
BenjaminGolden <b.j.golden@outlook.com>
2021-03-11 11:54:30 -0600

    added event listeners to all boxes

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,70 @@ import { getEntry } from "./DataManager.js"
 import { NavBar } from "./nav/NavBar.js"
 import { Footer } from "./nav/Footer.js"
 
+
+const applicationElement = document.querySelector(".dailyJournal");//application element stores the place on the dom we are listening.
+//adding an event listener to the date box
+applicationElement.addEventListener("change", event =>{
+    if(event.target.id === "journalDate"){
+        console.log("what day is it?")
+    }
+})
+
+//adding an event listener to the mood box
+applicationElement.addEventListener("change", event =>{
+    if(event.target.id === "mood"){
+        console.log("How ya feelin'?")
+    }
+})
+
+//adding an event listener to the concepts box
+applicationElement.addEventListener("click", event => {
+    if (event.target.id === "concepts"){
+        console.log("check out this concept")
+    }
+})
+
+//adding an event listener to the journal entry box
+applicationElement.addEventListener("click", event => {
+    if (event.target.id === "jEntry"){
+        console.log("whatcha thinkin'?")
+    }
+})
+//attempting to track any click on main...Not Working...
+applicationElement.addEventListener("click", (event) =>{
+    if(event.target.id === "main"){
+        console.log("clicked on main")
+    }
+})
+
+//listening for record journal entry button
+applicationElement.addEventListener("click", event =>{
+    if(event.target.id === "recordEntry"){
+        console.log("store my thoughts")
+    }
+})
+
+//listening for entry search in nav bar
+applicationElement.addEventListener("click", event =>{
+    if(event.target.id === "journalSearch"){
+        console.log("show me entries")
+    }
+})
+
+//
+applicationElement.addEventListener("click", event =>{
+    if(event.target.id === "logout"){
+        console.log("Bye 'Felicia!")
+    }
+})
+
+
+
 const startDailyJournal = () => {
     const journalElement = document.querySelector(".journalList");//setting journalElement = to where I want to put the HTML on the Dom
 	getEntry().then((entryData) => {//getEntry contains fetched data from json server. .then-uses the data stored in () 
     journalElement.innerHTML = EntryListComponent(entryData);//trying to grab the fetched data and display it through the HTML rep created in journalFetch
     //I am trying to set the information that I receive from the fetched data call equal to my HTML rep on the Dom.
-    console.log(getEntry);
     }) 
     
 }
--- scripts/nav/NavBar.js
+++ scripts/nav/NavBar.js
@@ -2,16 +2,16 @@ export const NavBar = () => {
     return `
         <nav class="navigation">
             <div class="navigation__item navigation__icon">
-                <img src="./images/pb.png" alt="" />
+                <img src="./images/brain.png" alt="brains" />
             </div>
             <div class="navigation__item navigation__name">
-                JournalEntry
+                The Golden Journal
             </div>
             <div class="navigation__item navigation__search">
-                <input type="text" id="postSearch" placeholder="Search posts..." />
+                <input type="text" id="journalSearch" placeholder="Search entries..." />
             </div>
             <div class="navigation__item navigation__message">
-                <img id="directMessageIcon" src="./images/fountain-pen.svg" alt="" />
+                <img id="directMessageIcon" src="" alt="" />
             </div>
             <div class="navigation__item navigation__logout">
                 <button id="logout" class="fakeLink">Logout</button>
