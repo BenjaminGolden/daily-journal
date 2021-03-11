4747a4cfe5463df448b0d2bca32733a45ef1feae
BenjaminGolden <b.j.golden@outlook.com>
2021-03-11 15:53:55 -0600

    filtered data showing up empty

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 import { EntryListComponent } from './JournalEntryList.js';
-import { getEntry } from "./DataManager.js"
+import { getEntry, useEntryCollection, createEntry } from "./DataManager.js"
 import { NavBar } from "./nav/NavBar.js"
 import { Footer } from "./nav/Footer.js"
+
 
 
 const applicationElement = document.querySelector(".dailyJournal");//application element stores the place on the dom we are listening.
@@ -32,9 +33,9 @@ applicationElement.addEventListener("click", event => {
         console.log("whatcha thinkin'?")
     }
 })
-//attempting to track any click on main...Not Working...
+//attempting to track any click on main
 applicationElement.addEventListener("click", (event) =>{
-    if(event.target.id === "main"){
+    if(event.target.id === ""){
         console.log("clicked on main")
     }
 })
@@ -53,12 +54,73 @@ applicationElement.addEventListener("click", event =>{
     }
 })
 
-//
+//logout button listener
 applicationElement.addEventListener("click", event =>{
     if(event.target.id === "logout"){
         console.log("Bye 'Felicia!")
     }
 })
+
+//edit button listener...the id returns undefined...
+applicationElement.addEventListener("click", event =>{
+    if(event.target.id.startsWith ("edit")){
+        console.log("entry clicked", event.target.id.split("--"))
+		console.log("the id is", event.target.id.split("--")[1])
+    }
+})
+        
+    const showFilteredEntries = (year) => {
+        //get a copy of the post collection
+        const epoch = Date.parse(`01/01/${year}`);
+        console.log(epoch);
+        //filter the data
+        debugger
+        const dataArray = useEntryCollection()
+        const filteredData = dataArray.filter(singleEntry => {
+        if (singleEntry.timestamp >= epoch) {
+            return singleEntry
+        }
+     });
+        const entryElement = document.querySelector(".entryList");
+        entryElement.innerHTML = EntryListComponent(filteredData);
+    }
+
+applicationElement.addEventListener("change", event => {
+  if (event.target.id === "yearSelection") {
+    const yearAsNumber = parseInt(event.target.value)
+    console.log(`User wants to see posts since ${yearAsNumber}`)
+    //invoke a filter function passing the year as an argument
+    showFilteredEntries(yearAsNumber);
+  }
+})
+
+
+
+applicationElement.addEventListener("click", event => {
+    event.preventDefault();//means don't refresh the page
+    if (event.target.id === "newPost__submit") {
+    //collect the input values into an object to post to the DB
+      const title = document.querySelector("input[name='postTitle']").value
+      const url = document.querySelector("input[name='postURL']").value
+      const description = document.querySelector("textarea[name='postDescription']").value
+      //we have not created a user yet - for now, we will hard code `1`.
+      //we can add the current time as well
+      const postObject = {
+          title: title,
+          imageURL: url,
+          description: description,
+          userId: getLoggedInUser().id,
+          timestamp: Date.now()
+      }
+  
+    // be sure to import from the DataManager
+        createPost(postObject)
+        .then(response => {
+            console.log("what is the new post", response);
+            showPostList();
+        })
+    }
+  })
 
 
 
--- scripts/nav/Footer.js
+++ scripts/nav/Footer.js
@@ -1,9 +1,15 @@
 export const Footer = () => {
-    return `
-    <footer>
-        <div>
-            &copy;B.J. Golden's thoughts, moods and feelings.
-        </div>
-    </footer>
+    return `             
+             <div class="footer__item">
+             &copy;B.J. Golden's thoughts, moods and feelings.
+                <div> 
+                    Posts since: <select id="yearSelection">
+                        <option>2021</option>
+                        <option>2020</option>
+                    </select>
+                    <span id="postCount">0</span>
+                </div>
+            </div>
+ 
     `
 }
